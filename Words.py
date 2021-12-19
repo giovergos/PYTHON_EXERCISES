bcb262a4a4026819310e683365dbf2f8d3418adf
Georgios Vergos <94325473+giovergos@users.noreply.github.com>
2021-12-19 16:14:56 +0200

    Update Words.py

--- Words.py
+++ Words.py
@@ -1,3 +1,6 @@
+#A program that reads an alphanumeric and checks if it is palindromic. If it is true, the program shows a dictionary named 'adict' that includes only one pair, having the alphanumeric as the key and its length as the value. Also it shows a list named 'alist' that includes as elements the characters of the alphanumeric.
+#If the alphanumeric isn't palindromic, it shows 'Δεν είναι παλίνδρομο' (means 'It is not palindromic') and stops. No need to check the input, consider that it is always capital letters and at least one character (letter or number).
+
 s = input('s = ')
 if len(s) == 1:
     print('Μήκος = 1')
