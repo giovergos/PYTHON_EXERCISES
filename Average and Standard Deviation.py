2689a46c6ff9526e950ce7174a898d425747b855
Georgios Vergos <94325473+giovergos@users.noreply.github.com>
2021-12-31 19:22:07 +0200

    Update Average and Standard Deviation.py

--- Average and Standard Deviation.py
+++ Average and Standard Deviation.py
@@ -1,3 +1,10 @@
+"""
+A program that reads a file named "inputdata.txt" and then:
+a) calculates the average number and the standard deviation of the readen values from "inputdata.txt"
+b) creates the file "outputdata.txt" and saves to it the values of the average number and the standard deviation
+the inputdata file is already existed and the values of the average number and the standard deviation have to be floats with 3 decimals.
+"""
+
 from statistics import mean, stdev
 
 with open('inputdata.txt', 'r') as f:
